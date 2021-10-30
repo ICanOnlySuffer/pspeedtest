56997ee490531e288863829177162470d8b5845e
ICanOnlySuffer <pierorojas@tutamail.com>
2021-10-30 10:04:44 -0500

    :bytes -> bps

--- lib/pocha-speed-test/result.rb
+++ lib/pocha-speed-test/result.rb
@@ -3,7 +3,7 @@ module PochaSpeedTest
 	Result = Struct.new :server, :download_speed, :upload_speed do
 		def to_s
 			"%s\n  Download speed: %s\n  Upload speed: %s" % [
-				server, download_speed.bytes, upload_speed.bytes
+				server, download_speed.bps, upload_speed.bps
 			]
 		end
 	end
