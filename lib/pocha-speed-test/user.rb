e17a60799adfa1fce611fdc60b2e8d35c10cd0ec
ICanOnlySuffer <pierorojas@tutamail.com>
2021-10-31 18:00:36 -0500

    added User.print

--- lib/pocha-speed-test/user.rb
+++ lib/pocha-speed-test/user.rb
@@ -1,5 +1,5 @@
 
-module PochaSpeedTest
+class PochaSpeedTest
 	module User
 		STRING_FORMAT = "User\n  IP: %s\n  Coords: %s"
 		@@geopoint = GeoPoint.new
@@ -16,9 +16,6 @@ module PochaSpeedTest
 		def self.geopoint= geopoint
 			@@geopoint = geopoint
 		end
-		def self.coords
-			"%.4f, %.4f" % @@geopoint.values
-		end
 		
 		def self.update
 			page = HTTParty.get "http://www.speedtest.net/speedtest-config.php"
@@ -28,6 +25,21 @@ module PochaSpeedTest
 			@@ip = ip
 			@@geopoint.latitude = latitude.to_f
 			@@geopoint.longitude = longitude.to_f
+		end
+		
+		def self.print mode: 0, spacing: "  ", decimals: 4
+			puts case mode
+			when 0, :normal
+				"User\n%sIP: %s%sCoords: %.#{decimals}f, %.#{decimals}f" % [
+					spacing, @@ip, spacing, *@@geopoint.values
+				]
+			when 1, :compact
+				"User: {IP: %s, Coords: [%.#{decimals}f %.#{decimals}f]}" % [
+					@@ip, @@geopoints.latitude, @@geopoint.longitude
+				]
+			else
+				"Error: printing mode #{mode} for User"
+			end
 		end
 	end
 end
