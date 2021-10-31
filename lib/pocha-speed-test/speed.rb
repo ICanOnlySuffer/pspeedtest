82245f24991b26f7b7c8d202d440dee3d2eba2c7
ICanOnlySuffer <pierorojas@tutamail.com>
2021-10-31 18:47:12 -0500

    added Speed.to_s

--- lib/pocha-speed-test/speed.rb
+++ lib/pocha-speed-test/speed.rb
@@ -1,8 +1,13 @@
 
 module PochaSpeedTest
 	Speed = Struct.new :time, :bytes do
-		def to_s
-			(bytes.sum * 8 / time).bps
+		def bytes_per_second
+			bytes.sum * 8 / time
+		end
+		def to_s decimals: 4
+			"Took %.#{decimals}f seconds to download %i bytes [%s]" % [
+				time, bytes.sum, bytes_per_second.bps
+			]
 		end
 	end
 end
