4051083ce8376110c74285b1101b6840ab875007
ICanOnlySuffer <pierorojas@tutamail.com>
2021-11-01 07:49:17 -0500

    now PochaSpeedTest is a class and not a module

--- lib/pocha-speed-test.rb
+++ lib/pocha-speed-test.rb
@@ -1,14 +1,40 @@
+require_relative "pocha-speed-test/version"
+require_relative "pocha-speed-test/blocks"
+
+require_relative "pocha-speed-test/server"
+require_relative "pocha-speed-test/user"
+
+require_relative "pocha-speed-test/speed"
+
 require "httparty"
 
-require_relative "pocha-speed-test/test-blocks"
-require_relative "pocha-speed-test/geo-point"
-require_relative "pocha-speed-test/version"
-require_relative "pocha-speed-test/result"
-require_relative "pocha-speed-test/server"
-require_relative "pocha-speed-test/bytes"
-require_relative "pocha-speed-test/speed"
-require_relative "pocha-speed-test/user"
-require_relative "pocha-speed-test/test"
+class PochaSpeedTest
+	@@download_sizes = [1_000] * 8
+	@@upload_sizes = [400_000] * 8
+	@@block = BLOCKS[:default]
+	@@ping = 4.times
+	
+	def run enumerator = 1.times
+		enumerator.each do
+			@block.call @download_sizes, @upload_sizes, @ping
+		end
+	end
+	
+	def on_run &block
+		@block = block
+	end
+	
+	def initialize ping: @@ping,
+			download_sizes: @@download_sizes,
+			upload_sizes: @@upload_sizes,
+			&block
+		
+		@download_sizes = download_sizes
+		@upload_sizes = upload_sizes
+		@block = block || @@block
+		@ping = ping
+	end
+end
 
 
 
