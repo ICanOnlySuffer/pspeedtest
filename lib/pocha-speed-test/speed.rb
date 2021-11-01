24458448e924348b0d25192aa1ab98cd53d6dbf7
ICanOnlySuffer <pierorojas@tutamail.com>
2021-11-01 07:55:13 -0500

    moved lib/pocha-speed-test/bytes.rb code and fixed MB and mbps missinterpretation

--- lib/pocha-speed-test/speed.rb
+++ lib/pocha-speed-test/speed.rb
@@ -1,15 +1,44 @@
 
-module PochaSpeedTest
+
+class Numeric
+	def bytes decimals: 2
+		string, max = {
+			"b"  => 1024,
+			"Kb" => 1048576,
+			"Mb" => 1073741824,
+			"Gb" => 1099511627776
+		}.find {|key, max|
+			max > self
+		}
+		
+		"%.#{decimals}f%s" % [self * 1024 / max, string]
+	end
+	
+	def bits_per_second
+		bytes.downcase + "ps"
+	end
+	
+	alias :bps :bits_per_second
+end
+
+class PochaSpeedTest
 	Speed = Struct.new :time, :bytes do
-		def bytes_per_second
-			bytes.sum * 8 / time
+		def bits
+			bytes * 8 / time
 		end
-		def to_s decimals: 4
+		
+		def to_s
+			bits.bps
+		end
+		
+		def details decimals: 4
 			"Took %.#{decimals}f seconds to download %i bytes [%s]" % [
-				time, bytes.sum, bytes_per_second.bps
+				time, bytes, bits.bps
 			]
 		end
 	end
 end
+
+
 
 
