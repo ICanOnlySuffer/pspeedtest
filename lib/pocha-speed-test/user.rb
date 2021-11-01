f13ccf05e0bdeeba7c2ba841597084f60d614740
ICanOnlySuffer <pierorojas@tutamail.com>
2021-11-01 07:56:06 -0500

    optimized function :update and renamed it as :update!

--- lib/pocha-speed-test/user.rb
+++ lib/pocha-speed-test/user.rb
@@ -1,46 +1,46 @@
 
 class PochaSpeedTest
-	module User
-		STRING_FORMAT = "User\n  IP: %s\n  Coords: %s"
-		@@geopoint = GeoPoint.new
+	module USER
+		
+		# pain
 		
 		def self.ip
 			@@ip
 		end
-		def self.ip= ip
-			@@ip = ip
+		def self.lat
+			@@lat
 		end
-		def self.geopoint
-			@@geopoint
-		end
-		def self.geopoint= geopoint
-			@@geopoint = geopoint
+		def self.lon
+			@@lon
 		end
 		
-		def self.update
-			page = HTTParty.get "http://www.speedtest.net/speedtest-config.php"
-			scan = page.body.match /ip="([^"]*)" lat="([^"]*)" lon="([^"]*)"/
-			ip, latitude, longitude = scan.captures
+		# not pain
+		
+		def self.update!
+			data = (
+				HTTParty.get "http://www.speedtest.net/speedtest-config.php"
+			) ["settings"]["client"]
 			
-			@@ip = ip
-			@@geopoint.latitude = latitude.to_f
-			@@geopoint.longitude = longitude.to_f
+			@@ip  = data ["ip"]
+			@@lat = data ["lat"].to_f
+			@@lon = data ["lon"].to_f
 			
-			User
+			USER # helps doing stuff like: puts User.update.to_s mode: compact
 		end
 		
-		def self.to_s mode: 0, spacing: "  ", decimals: 4
+		def self.to_s mode: :default, spacing: "  ", decimals: 4
 			case mode
-			when 0, :normal
-				"User\n%sIP: %s%sCoords: %.#{decimals}f, %.#{decimals}f" % [
-					spacing, @@ip, spacing, *@@geopoint.values
-				]
-			when 1, :compact
-				"User: {IP: %s, Coords: [%.#{decimals}f %.#{decimals}f]}" % [
-					@@ip, @@geopoints.latitude, @@geopoint.longitude
-				]
+			when :default
+				<<~TEXT % [spacing, @@ip, spacing, @@lat, @@lon]
+					%sIP: %s
+					%sCoords: %.#{decimals}f, %.#{decimals}f
+				TEXT
+			when :compact
+				<<~TEXT % [@@ip, @@lat, @@lon]
+					ip: %s, lat: %.#{decimals}, lon: %.#{decimals}
+				TEXT
 			else
-				"Error: no mode: %s for User.to_s" % mode.inspect
+				raise "Error: no mode: %s for User.to_s" % mode.inspect
 			end
 		end
 	end
