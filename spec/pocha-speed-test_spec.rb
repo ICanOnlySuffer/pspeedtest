2d9beec55ddb1dfc2972ac83ecd1c815c9cc4d34
ICanOnlySuffer <pierorojas@tutamail.com>
2021-11-02 23:37:32 -0500

    not important

--- spec/pocha-speed-test_spec.rb
+++ spec/pocha-speed-test_spec.rb
@@ -1,11 +1,23 @@
 # frozen_string_literal: true
 
 RSpec.describe PochaSpeedTest do
-  it "has a version number" do
-    expect(PochaSpeedTest::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+	
+	# it "Should update USER" do
+	# 	PochaSpeedTest::USER.update!
+	# 	
+	# 	
+	# end
+	
+	
+	# it "has a version number" do
+	# 	expect(PochaSpeedTest::VERSION).not_to be nil
+	# end
+	
+	# it "does something useful" do
+	# 	expect(false).to eq(true)
+	# end
 end
+
+
+
+
