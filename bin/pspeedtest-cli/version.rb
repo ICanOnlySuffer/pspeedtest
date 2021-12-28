107e230e5686ac141c9856a53f3d185cbe4fffd5
ICanOnlySuffer <pierorojas@tutamail.com>
2021-12-28 05:17:34 -0500

    re-added nickname

--- bin/pspeedtest-cli/version.rb
+++ bin/pspeedtest-cli/version.rb
@@ -1,11 +1,12 @@
 
-VERSION = "pspeedtest #{PSpeedTest::VERSION} - Piero Rojas\n" + {
+VERSION = "pspeedtest #{
+	PSpeedTest::VERSION
+} - Piero Rojas (ICanOnlySuffer)\n" + {
 	"en" => <<~TXT,
 		Adapted from petemyron's speedtest gem
 		  %{petemyron}
 		Which is a gemmed version of lacostej's speedtest.rb
 		  %{lacostej}
-		
 		Under MIT Licence (Jerome Lacoste)
 	TXT
 	"es" => <<~TXT,
@@ -13,7 +14,6 @@ VERSION = "pspeedtest #{PSpeedTest::VERSION} - Piero Rojas\n" + {
 		  %{petemyron}
 		La cual es una version gemada de speedtest.rb de lacostej
 		  %{lacostej}
-		
 		Bajo Licencia MIT (Jerome Lacoste)
 	TXT
 }.in_lang % {
