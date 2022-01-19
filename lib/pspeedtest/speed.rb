def80303be3644f8c5eca2110a98d91ff480af6f
ICanOnlySuffer <pierorojas@tutamail.com>
2022-01-19 07:25:44 -0500

    removed unnecesary code

--- lib/pspeedtest/speed.rb
+++ lib/pspeedtest/speed.rb
@@ -1,56 +1,22 @@
 
-class Numeric
-	def bytes decimals: 0
-		string, max = {
-			"b"  => 1024,
-			"Kb" => 1048576,
-			"Mb" => 1073741824,
-			"Gb" => 1099511627776
-		}.find {|key, max|
-			max > self
-		}
-		
-		"%.#{decimals}f%s" % [self * 1024 / max, string]
-	end
-	
-	def bits_per_second
-		(bytes decimals: 2).downcase + "ps" # tss
-	end
-	
-	alias :bps :bits_per_second
-end
-
-class Array
-	def average &block
-		size.zero? ? (block ? (sum block) : sum) / size : 0
-	end
-end
-
-class PSpeedTest
+module PSpeedTest
 	Speed = Struct.new :bits, :time do
-		def bps
-			(bits / time).bps
-		end
-		
-		def to_s debug: "%{bps}"
-			debug % {
-				bits: bits,
-				time: time,
-				bps: bps
+		def bps decimals: 2
+			bits_div_time = bits / time
+			
+			string, max = {
+				'bps'  => 1024,
+				'Kbps' => 1048576,
+				'Mbps' => 1073741824,
+				'Gbps' => 1099511627776
+			}.find {|key, max|
+				max > bits_div_time
 			}
+			
+			"%.#{decimals}f%s" % [bits / time * 1024 / max, string]
 		end
 		
-		def + speed
-			Speed.new self.bits + speed.bits, self.time
-		end
-		
-		def / amount
-			Speed.new self.bits / amount, self.time
-		end
-		
-		def self.average speeds
-			Speed.new (speeds.average &:bits), (speed.average &:time)
-		end
+		alias :to_s :bps
 	end
 end
 
