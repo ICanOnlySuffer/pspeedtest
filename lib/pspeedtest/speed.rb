bdf5cdfdb44d653ab55762900602a29f2533d2bb
ICanOnlySuffer <pierorojas@tutamail.com>
2022-01-20 21:58:41 -0500

    removed unncesary code

--- lib/pspeedtest/speed.rb
+++ lib/pspeedtest/speed.rb
@@ -1,22 +1,17 @@
 
 module PSpeedTest
 	Speed = Struct.new :bits, :time do
-		def bps decimals: 2
-			bits_div_time = bits / time
-			
-			string, max = {
+		def bps
+			div = bits.fdiv time
+			{
 				'bps'  => 1024,
 				'Kbps' => 1048576,
 				'Mbps' => 1073741824,
 				'Gbps' => 1099511627776
-			}.find {|key, max|
-				max > bits_div_time
+			}.each {|string, max|
+				return [div * 1024 / max, string] if max > div
 			}
-			
-			"%.#{decimals}f%s" % [bits / time * 1024 / max, string]
 		end
-		
-		alias :to_s :bps
 	end
 end
 
