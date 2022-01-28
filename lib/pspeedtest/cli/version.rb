6b0e9af4b0ae75dac69e49901ec3c91ddd533f70
ICanOnlySuffer <pierorojas@tutamail.com>
2022-01-28 10:05:45 -0500

    improved

--- lib/pspeedtest/cli/version.rb
+++ lib/pspeedtest/cli/version.rb
@@ -1,30 +1,12 @@
-require_relative 'lang'
+require_relative '../version'
 
-VERSION = {
-	'en' => <<~TXT,
-		Adapted from petemyron's speedtest gem
-		  %{petemyron}
-		Which is a gemmed version of lacostej's speedtest.rb
-		  %{lacostej}
-		Under MIT Licence
-	TXT
-	'es' => <<~TXT,
-		Adaptado de la gema speedtest de petemyron
-		  %{petemyron}
-		La cual es una version gemada de speedtest.rb de lacostej
-		  %{lacostej}
-		Bajo Licencia MIT
-	TXT
-}.translate
-
-def version
-	puts "pspeedtest v#{PSpeedTest::VERSION} - Piero Rojas"
-	puts VERSION % {
-		'petemyron': 'https://github.com/petemyron/speedtest/',
-		'lacostej': 'https://github.com/lacostej/speedtest.rb'
-	}
-	exit 0
+puts "pspeedtest v#{PSpeedTest::VERSION} - Piero Estéfano Rojas Effio"
+puts '> https://github.com/ICanOnlySuffer/pspeedtest'
+puts case ENV['LANG']
+when /es/ then 'Bajo licencia MIT'
+when /pt/ then 'Sob licença MIT'
+else 'Under MIT Licence'
 end
 
-
+exit
 
