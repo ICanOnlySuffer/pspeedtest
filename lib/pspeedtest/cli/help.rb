8658d9b5c7a8b95fe6170c9af7cfd620b9b432ab
ICanOnlySuffer <pierorojas@tutamail.com>
2022-01-28 10:06:01 -0500

    improved

--- lib/pspeedtest/cli/help.rb
+++ lib/pspeedtest/cli/help.rb
@@ -1,149 +1,97 @@
-require_relative 'lang'
 
-ALLOWED = {
-	download: '500, 750, 1000, 1500, 2000, 2500, 3000, 3500',
-	debug: (
-		[
-			'host (string)',
-			'sponsor (string)',
-			'latency (float) ',
-			'longitude (float)',
-			'distance (float)',
-			'distance.m (float)',
-			'distance.mi (float)',
-			'download.num (float)',
-			'download.str (string)',
-			'download.bits (integer)',
-			'download.time (float)',
-			'upload.num (float)',
-			'upload.str (string)',
-			'upload.bits (integer)',
-			'upload.time (float)'
-		].join "\n    "
-	)
-}
-
-EXAMPLE = {
-	download: '--download="500 1500 2500 3500"',
-	upload: '--upload="2000000 4000000 6000000 8000000"',
-	debug: '--debug="speed: %<download.num>.4f %{download.str}\n"',
-	file: '--file="~/Documents/internet.txt"'
-}
-
-USAGE = {
-	'en' => <<~TXT,
-		Usage: pspeedtest --OPTION=VALUE ...
-		Options:
-	TXT
-	'es' => <<~TXT,
-		Uso: pspeedtest --OPCIÓN=VALOR ...
-		Opciones:
-	TXT
-}.translate
-
-HELP = {
-	'en' => {
-		'download' => <<~TXT,
-			- Sizes of the images to download
-			- Allowed values:
-			    %{allowed.download}
-			- Example:
-			    %{example.download}
-		TXT
-		'upload' => <<~TXT,
-			- Sizes of the strings to upload
-			- Example:
-			    %{example.upload}
-		TXT
-		'debug' => <<~TXT,
-			- String to be formated
-			- Allowed values:
-			    %{allowed.debug}
-			- Example:
-			    %{example.debug}
-		TXT
-		'delay' => <<~TXT,
-			- Seconds to sleep between runs
-		TXT
-		'runs' => <<~TXT,
-			- Times to run the block
-			- An integer or 'inf' to run infinitely
-		TXT
-		'file' => <<~TXT,
-			- File in which the output will be appended.
-			- Example:
-			   %{example.file}
-		TXT
-		'rescue' => <<~TXT,
-			- Ommits errors
-			- By default: true
-		TXT
-	},
-	'es' => {
-		'download' => <<~TXT,
-			- Tamaños de las imágenes a descargar
-			- Valores permitidos:
-			    %{allowed.download}
-			- Ejemplo:
-			    %{example.download}
-		TXT
-		'upload' => <<~TXT,
-			- Tamaño de las cadenas a subir
-			- Ejemplo:
-			    %{example.upload}
-		TXT
-		'debug' => <<~TXT,
-			- Cadena a formatear
-			- Valores permitidos:
-			    %{allowed.debug}
-			- Ejemplo:
-				%{example.debug}
-		TXT
-		'delay' => <<~TXT,
-			- Tiempo a esperar entre corridas
-		TXT
-		'runs' => <<~TXT,
-			- Veces a correr el bloque
-			- Entero o 'inf' para correr indefinidamente
-		TXT
-		'file' => <<~TXT,
-			- Archivos al caul se le guardará el output
-			- Ejemplo:
-			    %{example.file}
-		TXT
-		'rescue' => <<~TXT,
-			- Omite errores
-			- Por defecto: true
-		TXT
-	}
-}.translate
-
-def usage option, description
-	puts "  --#{option}", (
-		description % {
-			'example.download': EXAMPLE[:download],
-			'example.upload': EXAMPLE[:upload],
-			'example.debug': EXAMPLE[:debug],
-			'example.file': EXAMPLE[:file],
-			'allowed.download': ALLOWED[:download],
-			'allowed.debug': ALLOWED[:debug]
-		}
-	).lines.map {|line|
-		'    ' + line
-	}
+case ENV['LANG']
+when /es/
+	USAGE = 'Uso'
+	VALUE = 'OPCIÓN=VALOR'
+	OPTIONS = 'Options'
+	ALLOWED = 'Valores permitidos'
+	EXAMPLE = 'Ejemplo'
+	DOWNLOAD = 'Tamaños de las imágenes a descargar'
+	UPLOAD = 'Tamaños de las cadenas a subir'
+	DEBUG = 'Cadena a ser formateada'
+	DELAY = 'Segundos entre iteraciones'
+	RUNS = 'Veces a correr el bucle (vacío para infinitas veces)'
+	FILE = 'Archivo en el cual se escribirá'
+when /pt/
+	USAGE = 'Uso'
+	VALUE = 'OPÇÃO=VALOR'
+	OPTIONS = 'Opções'
+	ALLOWED = 'Valores permitidos'
+	EXAMPLE = 'Exemplo'
+	DOWNLOAD = 'Tamanhos das imagens para download'
+	UPLOAD = 'Tamanhos de string para upload'
+	DEBUG = 'String a ser formatada'
+	DELAY = 'Segundos entre as iterações'
+	RUNS = 'Vezes para executar o loop (vazio por infinitas vezes)'
+	FILE = 'Arquivo para gravar'
+else
+	USAGE = 'Usage'
+	VALUE = 'OPTION=VALOR'
+	OPTIONS = 'Options'
+	ALLOWED = 'Allowed values'
+	EXAMPLE = 'Example'
+	DOWNLOAD = 'Sizes of the images to download'
+	UPLOAD = 'Sizes of the strings to uplload'
+	DEBUG = 'String to be formated'
+	DELAY = 'Seconds between iterations'
+	RUNS = 'Times to run the loop (emtpy for infinite times)'
+	FILE = 'Archivo en el cual se escribirá'
 end
 
-def help value
-	if HELP[value]
-		usage value, HELP[value]
-		exit 0
-	end
-	
-	puts USAGE
-	HELP.each {|option, description|
-		usage option, description
-	}
-	exit value ? 1 : 0
-end
+puts <<~TXT
+	#{USAGE}: pspeedtest [#{VALUE}...]
+	#{OPTIONS}:
+	  --download=ARRAY  #{DOWNLOAD}.
+	                    #{ALLOWED}:
+	                      500 1000 1500 2000 2500 3000 3500 4000
+	                    #{EXAMPLE}:
+	                      --download='500 1500 2500 3500'
+	  --upload=ARRAY    #{UPLOAD}.
+	                    #{EXAMPLE}:
+	                      --upload='2000000 4000000 6000000 8000000'
+	  --debug=STRING    #{DEBUG}.
+	                    #{ALLOWED}:
+	                      host (string)
+	                      sponsor (string)
+	                      latency (float)
+	                      longitude (float)
+	                      distance (float)
+	                      distance.m (float)
+	                      distance.mi (float)
+	                      total.download.num (float)
+	                      total.download.str (string)
+	                      total.download.bits (integer)
+	                      total.download.time (float)
+	                      total.upload.num (float)
+	                      total.upload.str (string)
+	                      total.upload.bits (integer)
+	                      total.upload.time (float)
+	                      download.num (float)
+	                      download.str (string)
+	                      download.bits (integer)
+	                      download.time (float)
+	                      upload.num (float)
+	                      upload.str (string)
+	                      upload.bits (integer)
+	                      upload.time (float)
+	                    #{EXAMPLE}:
+	                      --debug='%<upload.num>.4f %{upload.str}\\n'
+	  --delay=NUMBER    #{DELAY}.
+	                    #{EXAMPLE}:
+	                      --delay=20
+	  --runs=NUMBER     #{RUNS}.
+	                    #{EXAMPLE}:
+	                      --runs=20
+	  --file=STRING     #{FILE}.
+	                    #{EXAMPLE}:
+	                      --file='~/Documents/internet.log'
+	#{EXAMPLE}:
+	  $ pspeedtest \\
+	    --download='4000 4000 4000 4000' \\
+	    --debug='%{time}: %<download.num>.4f %{download.str}\\n' \\
+	    --file='internet.log' \\
+	    --runs=20
+TXT
 
+exit
 
