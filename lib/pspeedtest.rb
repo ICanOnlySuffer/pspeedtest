cf2febf4f04a3025ed5ad6be4784bcf8e223ce72
ICanOnlySuffer <pierorojas@tutamail.com>
2022-02-06 15:18:17 -0500

    minor improvements

--- lib/pspeedtest.rb
+++ lib/pspeedtest.rb
@@ -1,6 +1,8 @@
 require 'net/http'
 
 module PSpeedTest
+	VERSION = '2.3.0'
+	
 	Speed = Struct.new :bits, :time do
 		def bps
 			case div = (bits.fdiv time)
@@ -12,17 +14,15 @@ module PSpeedTest
 		end
 	end
 	
-	SERVER_FETCH_URI_URL = URI (
-		'https://c.speedtest.net/speedtest-servers.php'
-	)
-	SERVER_DOWNLOAD_URL = 'http://%s/speedtest/random%ix%i.jpg'
-	SERVER_UPLOAD_URL = 'http://%s/speedtest/upload.php'
-	SERVER_PING_URL = 'http://%s/speedtest/latency.txt'
+	FETCH_URI = URI 'https://c.speedtest.net/speedtest-servers.php'
+	DOWNLOAD_URL = 'http://%s/speedtest/random%ix%i.jpg'
+	UPLOAD_URL = 'http://%s/speedtest/upload.php'
+	PING_URL = 'http://%s/speedtest/latency.txt'
 	
 	Server = Struct.new :lat, :lon, :sponsor, :host, :latency do
 		def download sizes
 			threads = sizes.map {|size|
-				uri_url = URI SERVER_DOWNLOAD_URL % [host, size, size]
+				uri_url = URI DOWNLOAD_URL % [host, size, size]
 				Thread.new do
 					(Net::HTTP.get uri_url).length
 				end
@@ -33,7 +33,7 @@ module PSpeedTest
 		end
 		
 		def upload sizes
-			uri_url = URI SERVER_UPLOAD_URL % host
+			uri_url = URI UPLOAD_URL % host
 			threads = sizes.map {|size|
 				Thread.new do
 					(
@@ -51,7 +51,7 @@ module PSpeedTest
 				(
 					(
 						(
-							Net::HTTP.get SERVER_FETCH_URI_URL
+							Net::HTTP.get FETCH_URI
 						).force_encoding 'UTF-8'
 					).scan /(?:lat|lon|sponsor|host)="([^"]+)"/
 				).flatten.each_slice 4
@@ -64,25 +64,22 @@ module PSpeedTest
 			threads = self.fetch.map {|server|
 				thread = Thread.new do
 					start = Time.now
-					Net::HTTP.get (URI SERVER_PING_URL % server.host)
+					Net::HTTP.get (URI PING_URL % server.host)
 					server.latency = (Time.now - start) * 100
 					(threads - [thread]).each &:kill
 					server
 				end
 			}
 			
-			(threads.find &:value).value
+			(threads.find &:value).value rescue Hash.new
 		end
 	end
 	
 	SERVER = Server.new
-	
 	def SERVER.update!
 		Server.get_best.each_pair {|name, value|
 			SERVER[name] = value
 		}
-		
-		SERVER
 	end
 end
 
