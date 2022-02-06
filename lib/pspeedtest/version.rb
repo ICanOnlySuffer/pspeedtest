5224f7451d84c9d04e78abad5faff6c48062743d
ICanOnlySuffer <pierorojas@tutamail.com>
2022-02-06 15:15:00 -0500

    moved and updated

--- lib/pspeedtest/version.rb
+++ lib/pspeedtest/version.rb
@@ -1,6 +1,12 @@
+require_relative '../pspeedtest'
 
-module PSpeedTest
-	VERSION = '2.2.0'
+puts "pspeedtest v#{PSpeedTest::VERSION} - Piero Estéfano Rojas Effio"
+puts '* https://github.com/ICanOnlySuffer/pspeedtest'
+puts case ENV['LANG']
+when /es/ then 'Bajo licencia MIT'
+when /pt/ then 'Sob licença MIT'
+else 'Under MIT Licence'
 end
 
+exit
 
